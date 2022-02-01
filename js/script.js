3dbb6f38199957e55e5fe136468dd394398a6ee6
e duardo <edureis-14@hotmail.com>
2022-02-01 10:32:44 -0300

    Js adicionado

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function menuToggle(){
+    let menuArea =document.getElementById("menu_area");
+
+    if(menuArea.style.width == '0px') {
+         menuArea.style.width = '200px';
+    }else if 
+         (menuArea.style.width == '200px'){
+            menuArea.style.width  ='0px';
+         }
+    }
+       
+  
+
